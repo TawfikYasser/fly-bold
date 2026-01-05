a111c0e8b587283a2c1d845d25df9a11115e0dae
Anirudh Sengar <anirudhsengar3@gmail.com>
2026-01-05 20:38:45 +0400

    feat: configure client deployments with increased shared memory and YOLO dataset target path and update FEDN packages

--- fedn/deploy-application.sh
+++ fedn/deploy-application.sh
@@ -143,8 +143,10 @@ services:
     command: ["/bin/bash", "-c", "apt-get update && apt-get install -y libgl1-mesa-glx libglib2.0-0 libsm6 libxext6 libxrender-dev && export HOME=/app/tmp && mkdir -p /app/tmp && /venv/bin/pip install --no-cache-dir fedn torch==2.4.1 torchvision==0.19.1 'numpy<2' yolov5==7.0.14 'matplotlib>=3.2.2' opencv-python-headless==4.9.0.80 'Pillow>=7.1.2' 'PyYAML>=5.3.1' 'requests>=2.23.0' 'huggingface-hub>=0.24.0,<0.25.0' 'scipy>=1.4.1' 'tqdm>=4.64.0' 'pandas>=1.1.4' 'seaborn>=0.11.0' psutil 'thop>=0.1.1' 'protobuf>=5.0.0,<6.31.0' 'pycocotools>=2.0.6' && /venv/bin/fedn client start --combiner ${SERVER_INTERNAL_IP} --combiner-port 12080 -in client/fedn.yaml --name client-${CLIENT_ID_1} --local-package"]
     extra_hosts:
       - "combiner:${SERVER_INTERNAL_IP}"
+    shm_size: '4gb'
     environment:
       FEDN_CLIENT_ID: ${CLIENT_ID_1}
+      YOLO_SPLITS_TARGET: /app/datasets/coco_partitions
     volumes:
       - ./fly-bold-fedn/client:/app/client
       - ../logs:/app/logs
@@ -158,8 +160,10 @@ services:
     command: ["/bin/bash", "-c", "apt-get update && apt-get install -y libgl1-mesa-glx libglib2.0-0 libsm6 libxext6 libxrender-dev && export HOME=/app/tmp && mkdir -p /app/tmp && /venv/bin/pip install --no-cache-dir fedn torch==2.4.1 torchvision==0.19.1 'numpy<2' yolov5==7.0.14 'matplotlib>=3.2.2' opencv-python-headless==4.9.0.80 'Pillow>=7.1.2' 'PyYAML>=5.3.1' 'requests>=2.23.0' 'huggingface-hub>=0.24.0,<0.25.0' 'scipy>=1.4.1' 'tqdm>=4.64.0' 'pandas>=1.1.4' 'seaborn>=0.11.0' psutil 'thop>=0.1.1' 'protobuf>=5.0.0,<6.31.0' 'pycocotools>=2.0.6' && /venv/bin/fedn client start --combiner ${SERVER_INTERNAL_IP} --combiner-port 12080 -in client/fedn.yaml --name client-${CLIENT_ID_2} --local-package"]
     extra_hosts:
       - "combiner:${SERVER_INTERNAL_IP}"
+    shm_size: '4gb'
     environment:
       FEDN_CLIENT_ID: ${CLIENT_ID_2}
+      YOLO_SPLITS_TARGET: /app/datasets/coco_partitions
     volumes:
       - ./fly-bold-fedn/client:/app/client
       - ../logs:/app/logs
