92145d564b78f514acc4aeeb10a4b8c78b2ab8e4
Tawfik Yasser <tawfik@Tawfiks-MacBook-Air.local>
2026-07-06 01:08:03 +0300

    EXP: 101033230006 DONE

--- flower/experiments_outputs/analyzer.py
+++ flower/experiments_outputs/analyzer.py
@@ -66,7 +66,7 @@ def get_env(key, default, type_cast=str):
 
 
 # ==================== CONFIGURATION ====================
-INPUT_FILE = "/Users/tawfik/DeFeC3/flybold/flower/experiments_outputs/101033230003/EXP_YOLOv5_s_detection_101033230003_logs.json"
+INPUT_FILE = "/Users/tawfik/DeFeC3/flybold/flower/experiments_outputs/101033230006/EXP_YOLOv5_s_detection_101033230006_logs.json"
 
 # Extract experiment number
 match = re.search(r"_detection_(\d+)_", INPUT_FILE)
